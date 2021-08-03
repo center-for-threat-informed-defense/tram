c4172322c21462397ce38993ee2f68f1a51ef84a
Vohaska, Brian <bvohaska@mitre.org>
2021-08-03 12:31:46 -0400

    modified Django ALLOWED_HOSTS so and associated ENV can be imported as a configuration option

--- src/tram/tram/settings.py
+++ src/tram/tram/settings.py
@@ -12,6 +12,7 @@ https://docs.djangoproject.com/en/3.1/ref/settings/
 
 import os
 from pathlib import Path
+from json import loads
 
 # Build paths inside the project like this: BASE_DIR / 'subdir'.
 BASE_DIR = Path(__file__).resolve().parent.parent
@@ -38,7 +39,10 @@ if os.environ.get('DEBUG') != None:
 else:
     DEBUG = True
 
-ALLOWED_HOSTS = []
+if os.environ.get('ALLOWED_HOSTS') != None:
+    ALLOWED_HOSTS = loads(os.environ.get('ALLOWED_HOSTS'))
+else:
+    ALLOWED_HOSTS = []
 
 DATA_UPLOAD_MAX_NUMBER_FIELDS = None
 
