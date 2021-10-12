1ef00c321763cec8c41c31f130cfd804fd4de1ba
Mark Wilmes <mwilmes@mitre.org>
2021-10-12 08:27:22 -0600

    Add full report model

--- src/tram/tram/ml/base.py
+++ src/tram/tram/ml/base.py
@@ -23,6 +23,16 @@ from sklearn.model_selection import train_test_split
 from sklearn.naive_bayes import MultinomialNB
 from sklearn.pipeline import Pipeline
 
+from sklearn.base import BaseEstimator, TransformerMixin
+from sklearn.pipeline import Pipeline
+from sklearn.feature_selection import chi2, SelectPercentile
+from nltk.stem.snowball import EnglishStemmer
+from nltk.corpus import stopwords
+from sklearn.multiclass import OneVsRestClassifier
+from sklearn.svm import LinearSVC
+from nltk import word_tokenize
+from sklearn.feature_extraction import text as tsk
+
 # The word model is overloaded in this scope, so a prefix is necessary
 from tram import models as db_models
 
@@ -305,12 +315,99 @@ class LogisticRegressionModel(SKLearnModel):
             ("clf", LogisticRegression())
         ])
 
+class FullReportModel(SKLearnModel):
+    
+    class TextSelector(BaseEstimator, TransformerMixin):
+        """
+        Transformer to select a single column from the data frame to perform additional transformations on
+        Use on text columns in the data
+        """
+        def __init__(self, key):
+            self.key = key
+
+        def fit(self, X, y=None):
+            return self
+
+        def transform(self, X):
+            return X
+
+    
+    def clean_text(text):
+        """
+        Cleaning up the words contractions, unusual spacing, non-word characters and any computer science
+        related terms that hinder the classification.
+        """
+        text = str(text)
+        text = text.lower()
+        text = re.sub("\r\n", "\t", text)
+        text = re.sub(r"what's", "what is ", text)
+        text = re.sub(r"\'s", " ", text)
+        text = re.sub(r"\'ve", " have ", text)
+        text = re.sub(r"can't", "can not ", text)
+        text = re.sub(r"n't", " not ", text)
+        text = re.sub(r"i'm", "i am ", text)
+        text = re.sub(r"\'re", " are ", text)
+        text = re.sub(r"\'d", " would ", text)
+        text = re.sub(r"\'ll", " will ", text)
+        text = re.sub(r"\'scuse", " excuse ", text)
+        text = re.sub('(?:(?:25[0-5]|2[0-4][0-9]|[01]?[0-9][0-9]?)\.)\{3\}(?:25[0-5] |2[0-4][0-9]|[01]?[0-9][0-9]?)(/([0-2][0-9]|3[0-2]|[0-9]))?', 'IPv4', text)
+        text = re.sub('\b(CVE\-[0-9]{4}\-[0-9]{4,6})\b', 'CVE', text)
+        text = re.sub('\b([a-z][_a-z0-9-.]+@[a-z0-9-]+\.[a-z]+)\b', 'email', text)
+        text = re.sub('\b(\d{1,3}\.\d{1,3}\.\d{1,3}\.\d{1,3})\b', 'IP', text)
+        text = re.sub('\b([a-f0-9]{32}|[A-F0-9]{32})\b', 'MD5', text)
+        text = re.sub('\b((HKLM|HKCU)\\[\\A-Za-z0-9-_]+)\b', 'registry', text)
+        text = re.sub('\b([a-f0-9]{40}|[A-F0-9]{40})\b', 'SHA1', text)
+        text = re.sub('\b([a-f0-9]{64}|[A-F0-9]{64})\b', 'SHA250', text)
+        text = re.sub('http(s)?:\\[0-9a-zA-Z_\.\-\\]+.', 'URL', text)
+        text = re.sub('CVE-[0-9]{4}-[0-9]{4,6}', 'vulnerability', text)
+        text = re.sub('[a-zA-Z]{1}:\\[0-9a-zA-Z_\.\-\\]+', 'file', text)
+        text = re.sub('\b[a-fA-F\d]{32}\b|\b[a-fA-F\d]{40}\b|\b[a-fA-F\d]{64}\b', 'hash', text)
+        text = re.sub('x[A-Fa-f0-9]{2}', ' ', text)
+        text = re.sub('\W', ' ', text)
+        text = re.sub('\s+', ' ', text)
+        text = text.strip(' ')
+        return text
+    
+    
+    class StemTokenizer(object):
+        def __init__(self):
+            self.st = EnglishStemmer()
+            
+        def __call__(self, doc):
+            return [self.st.stem(t) for t in word_tokenize(doc)]
+    
+
+    def _preprocess_text(self, sentences):
+        lemma = nltk.stem.WordNetLemmatizer()
+        preprocessed_sentences = ""
+        for sentence in sentences:
+            # Lemmatize each word in sentence
+            sentence = ' '.join([lemma.lemmatize(w) for w in sentence.rstrip().split()])
+            sentence = re.sub(r'\d+', '', sentence)  # Remove digits with regex
+            preprocessed_sentences = preprocessed_sentences + ' ' + sentence
+        return [preprocessed_sentences]
+
+    def _sentence_tokenize(self, text):
+        return [text]
+
+    def get_model(self):
+        stop_words = stopwords.words('english')
+        new_stop_words = ["'ll", "'re", "'ve", 'ha', 'wa',"'d", "'s", 'abov', 'ani', 'becaus', 'befor', 'could', 'doe', 'dure', 'might', 'must', "n't", 'need', 'onc', 'onli', 'ourselv', 'sha', 'themselv', 'veri', 'whi', 'wo', 'would', 'yourselv']
+        stop_words.extend(new_stop_words)
+        return Pipeline([
+            ('columnselector', self.TextSelector(key = 'processed')),
+            ('tfidf', tsk.TfidfVectorizer(tokenizer = self.StemTokenizer(), stop_words = stop_words)),
+            ('selection', SelectPercentile(chi2, percentile = 50)),
+            ('classifier', OneVsRestClassifier(LinearSVC(penalty = 'l2', loss = 'squared_hinge', dual = False, max_iter = 1000, class_weight = 'balanced'), n_jobs = -1))
+        ])
+
 
 class ModelManager(object):
     model_registry = {  # TODO: Add a hook to register user-created models
         'dummy': DummyModel,
         'nb': NaiveBayesModel,
         'logreg': LogisticRegressionModel,
+        'fullreport': FullReportModel,
     }
 
     def __init__(self, model):
