4d5fc2b569f4d64f5ba078d898c5ee096f64a457
Mark Wilmes <mwilmes@mitre.org>
2021-11-23 08:41:26 -0700

    update object count

--- tests/tram/test_commands.py
+++ tests/tram/test_commands.py
@@ -64,7 +64,7 @@ class TestLoadData:
         # Assert
         assert Sentence.objects.count() == 1
         assert Report.objects.filter(ml_model='fullreport').count() == 1
-        assert Mapping.objects.count() == 15
+        assert Mapping.objects.count() == 7
 
 
 @pytest.mark.django_db
