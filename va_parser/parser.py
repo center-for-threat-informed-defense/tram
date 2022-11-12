1d66b1bb328b62068e772aaa9c2f9f1e55258cb7
Ankit Chauhan <ankit.c@safe.security>
2022-11-12 10:11:38 +0530

    added new function to analyse and process trade_web's CVE

--- va_parser/parser.py
+++ va_parser/parser.py
@@ -33,6 +33,99 @@ import requests
 #         json.dump(cve_data, json_file, indent=4)
 
 
+def cveDataModification():
+
+    path = os.path.join(
+        pathlib.Path(__file__).parent.resolve(), "../va_parser/cve-data/"
+    )
+
+    files_list = os.listdir(path)
+
+    for single_file in files_list:
+        if ".json" in single_file:
+            file_path = os.path.join(path, single_file)
+
+            with open(file_path, "r") as f:
+                cve_content = json.load(f)
+
+            for i in range(len(cve_content["ttpMapping"])):
+                cve_content["ttpMapping"][i]["confidence"] = cve_content["ttpMapping"][
+                    i
+                ]["confidence"]
+                cve_content["ttpMapping"][i] = {
+                    "techniqueId": cve_content["ttpMapping"][i]["technique_id"],
+                    "confidence": cve_content["ttpMapping"][i]["confidence"],
+                    "techniqueName": cve_content["ttpMapping"][i]["technique_name"],
+                }
+
+            with open(
+                file_path,
+                "w",
+            ) as json_file:
+                json.dump(cve_content, json_file, indent=4)
+            print(file_path)
+
+
+def get_empty_count():
+    count = 0
+    empty_list = []
+    path = os.path.join(
+        pathlib.Path(__file__).parent.resolve(), "../va_parser/cve-data/"
+    )
+
+    files_list = os.listdir(path)
+
+    for single_file in files_list:
+        if ".json" in single_file:
+            file_path = os.path.join(path, single_file)
+
+            with open(file_path, "r") as f:
+                cve_content = json.load(f)
+
+            if cve_content["ttpMapping"] == []:
+                count += 1
+                empty_list.append(single_file)
+    print(empty_list)
+    print("Total empty CVEs : ", count)
+
+
+def analyse_unique_cve():
+    count = 0
+    empty_list = []
+    path = os.path.join(
+        pathlib.Path(__file__).parent.resolve(), "../va_parser/cve-data/"
+    )
+    with open("/Users/ankitchauhan/Drive/tram/va_parser/unique-cve-list.txt", "r") as f:
+        unique_list = f.readlines()
+
+    for single_file in unique_list:
+        single_file = single_file.split("\n")[0] + ".json"
+        print(single_file)
+        file_path = os.path.join(path, single_file)
+
+        try:
+
+            with open(file_path, "r") as f:
+                cve_content = json.load(f)
+        except FileNotFoundError:
+            print("File Not Found")
+            continue
+
+        if cve_content["ttpMapping"] == []:
+            count += 1
+            single_file = single_file.split(".json")[0] + "\n"
+            empty_list.append(single_file)
+    print(empty_list)
+    with open(
+        "/Users/ankitchauhan/Drive/tram/va_parser/not_have_ttp.txt",
+        "w",
+        encoding="utf-8",
+    ) as f:
+        f.writelines(empty_list)
+
+    print("Total empty CVEs : ", count)
+
+
 def get_technique_name(technqiue_id):
 
     attack_path = os.path.join(
@@ -59,7 +152,7 @@ def cleanup():
 
     response = requests.request("GET", url, headers=headers, data=payload)
 
-    print(response.text)
+    print(response.content)
 
 
 def get_access_token():
@@ -75,9 +168,80 @@ def get_access_token():
     return response["access"]
 
 
+def trade_web_missing_ttp():
+
+    qwert_list = []
+    temp = {}
+
+    with open("/Users/ankitchauhan/Drive/tram/va_parser/not_have_ttp.txt", "r") as f:
+        unique_list = f.readlines()
+
+    with open("/Users/ankitchauhan/Documents/va_parser/tsar.json", "r") as f:
+        tsar_json = json.load(f)
+
+    url = "http://localhost:8000/techniques/"
+    count = 0
+    for single_cve in unique_list:
+        cve_id = single_cve.split("\n")[0]
+        if cve_id in tsar_json.keys():
+            cve_desc = tsar_json[cve_id]["title"]
+
+            payload = json.dumps(
+                [
+                    {
+                        "unique_identifier": cve_id,
+                        "description": [
+                            {
+                                "field_data": cve_desc,
+                                "field_type": "control",
+                            }
+                        ],
+                        "techniqueMapping": "",
+                    }
+                ]
+            )
+            headers = {
+                "Authorization": "Bearer " + get_access_token(),
+                "Content-Type": "application/json",
+            }
+
+            response = requests.request("POST", url, headers=headers, data=payload)
+
+            ttp_mapping = json.loads(response.content)[0]["technique_list"]
+
+            final_json = tsar_json[cve_id]
+            ttp_list = []
+
+            for single_technique in ttp_mapping:
+                temp_dict = single_technique
+                technique_id = temp_dict["technique_id"]
+                temp_dict["technique_name"] = get_technique_name(technique_id)
+                ttp_list.append(temp_dict)
+
+            final_json["ttpMapping"] = ttp_list
+
+            out_path = os.path.join(
+                pathlib.Path(__file__).parent.resolve(),
+                "../va_parser/trade-web-missing/",
+            )
+
+            with open(
+                out_path + cve_id + ".json",
+                "w",
+            ) as json_file:
+                json.dump(final_json, json_file, indent=4)
+            count += 1
+            print(". . . . . ", count, " . . . . . ")
+            print("Successfully wrote data for. . . ", cve_id)
+
+
+trade_web_missing_ttp()
+print("LAST . . . .. . . . ")
+input()
+
 year = 2002
 
-for year in range(2008, 2015, 1):
+for year in range(2017, 2020, 1):
 
     count = 0
 
